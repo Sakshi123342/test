c3a5ad771dff36b2c99427b25c40a2c2b8f6ff0a
Sakshi123342 <savnersakshi8@gmail.com>
2025-10-27 14:51:08 +0530

    First push

---
+++ main.js
@@ -0,0 +1,21 @@
+
+//function showWishes() {
+  //document.getElementById("wish").style.display = "block";
+
+  // ✅ Initialize Swiper after showing the wish section
+  //var swiper = new Swiper(".mySwiper", {
+   // loop: true,
+   // navigation: {
+      //nextEl: ".swiper-button-next",
+     // prevEl: ".swiper-button-prev",
+    //},
+    //pagination: {
+      //el: ".swiper-pagination",
+      //clickable: true,
+    //},
+   // autoplay: false,
+    // // delay: 3000,
+     // //disableOnInteraction: false,
+    
+ // });
+//}
